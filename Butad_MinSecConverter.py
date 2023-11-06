18925af911858a285c7127b842634d510240c53a
Esteve02 <144907076+Esteve02@users.noreply.github.com>
2023-11-06 22:03:41 +0800

    Add files via upload

---
+++ Butad_MinSecConverter.py
@@ -0,0 +1,5 @@
+
+variableSecond = eval(input("Enter seconds here:"))
+variableMinute = variableSecond // 60
+variableMinute1 = variableSecond % 60
+print(variableSecond, "seconds is", variableMinute, "minute/s and", variableMinute1, "seconds" )
